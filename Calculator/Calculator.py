62197296728eb163be9891629c4640991134a39a
curiousbasil <faisukkar@gmail.com>
2019-11-03 14:10:19 -0500

    Feature : Added Addition, Subtraction, Multiplication, Division, Squared and Sqrt files in Calculator Module. Update: Calculator file in Calculator Module

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,5 +1,9 @@
 from Calculator.Subtraction import subtraction
 from Calculator.Addition import addition
+from Calculator.Multiplication import multiplication
+from Calculator.Division import division
+from Calculator.Squared import squared
+from Calculator.Sqrt import sqrt
 
 
 class Calculator:
@@ -13,73 +17,21 @@ class Calculator:
         return self.result
 
     def subtract(self, a, b):
-        self.result = subtraction(a, b)
-        return self.resultA
-
-
-
-def addition(a, b):
-    return a + b
-
-
-def subtraction(a, b):
-    return a - b
-
-
-def multiplication(a, b):
-    return a * b
-
-
-def division(a, b):
-    return a / b
-
-
-def squared(a):
-    return a ** 2
-
-
-def sqrt(a):
-    return a ** 0.5
-
-
-class Calculator:
-    result = 0
-
-    def __init__(self):
-        x = 2 + 2
-        self.result = x;
-        pass
-
-    def add(self, a, b):
-        a = float(a)
-        b = float(b)
-        self.result = addition(a, b)
-        return self.result
-
-    def subtract(self, a, b):
-        a = float(a)
-        b = float(b)
         self.result = subtraction(a, b)
         return self.result
 
     def multiply(self, a, b):
-        a = float(a)
-        b = float(b)
         self.result = multiplication(a, b)
         return self.result
 
-    def divide(self, a, b):
-        a = float(a)
-        b = float(b)
+    def division(self, a, b):
         self.result = division(a, b)
         return self.result
 
-    def squared(self, a, ):
-        a = float(a)
+    def squared(self, a):
         self.result = squared(a)
         return self.result
 
-    def sqrt(self, a, ):
-        a = float(a)
+    def sqrt(self, a):
         self.result = sqrt(a)
         return self.result
