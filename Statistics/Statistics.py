1335bfece2ea91451079105e0e333696fa5a9f3f
curiousbasil <faisukkar@gmail.com>
2019-11-16 20:59:28 -0500

    Feature: Added mode Module Task : Added mode in unit test file Task: Added mode in Statistics File Task: Tested Mean, Median and Mode Successful

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -1,6 +1,7 @@
 from Calculator.Calculator import Calculator
 from Statistics.Mean import mean
 from Statistics.Median import median
+from Statistics.Mode import  mode
 from CSVReader.CsvReader import CsvReader
 
 
@@ -17,3 +18,8 @@ class Statistics(Calculator):
     def median(self, val1, val2, val3, val4, val5, val6, val7, val8, val9):
         self.result = median(val1, val2, val3, val4, val5, val6, val7, val8, val9)
         return self.result
+
+    def mode(self, val1, val2, val3, val4, val5, val6, val7, val8, val9, val10):
+        self.result = mode(val1, val2, val3, val4, val5, val6, val7, val8, val9, val10)
+        return self.result
+
